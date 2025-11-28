a646927e0d573b5bba63af877aa435261186f781
rishabh12j <rishabh12j@gmail.com>
2025-11-28 16:24:07 +0000

    initial code

--- src/rx200_moveit_control/setup.py
+++ src/rx200_moveit_control/setup.py
@@ -1,31 +1,34 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+from glob import glob
+import os
 
 package_name = 'rx200_moveit_control'
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Install launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        # Install config files
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='James Florin Petri',
-    maintainer_email='james.petri@mu.ie',
-    description='Package used for RX200 arm control via the moveit interface',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    maintainer='master26',
+    maintainer_email='rishabh.jain.2026@mumail.ie',
+    description='RX200',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'rx200_pick_place_client=rx200_moveit_control.rx200_pick_place_client:main',
-            'rx200_moveit_client=rx200_moveit_control.rx200_moveit_action_client:main'
+            'vision_node = rx200_moveit_control.vision_node:main',
+            'manipulation_node = rx200_moveit_control.manipulation_node:main',
+            'coordinator_node = rx200_moveit_control.coordinator_node:main',
         ],
     },
 )
